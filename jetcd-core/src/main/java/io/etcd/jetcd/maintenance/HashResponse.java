ae1d8fb45389dd7e87cb9c52f5c7a16a86e58361
Luca Burgazzoli <lburgazzoli@gmail.com>
2025-12-22 12:50:48 +0100

    feat: add HashResponse class for Maintenance API
    - Create HashResponse extending AbstractResponse
    - Wraps gRPC HashResponse with getHash() method
    - Used for consistency checking of KV store state

---
+++ jetcd-core/src/main/java/io/etcd/jetcd/maintenance/HashResponse.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016-2021 The jetcd authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.etcd.jetcd.maintenance;
+
+import io.etcd.jetcd.Maintenance;
+import io.etcd.jetcd.impl.AbstractResponse;
+
+/**
+ * HashResponse returned by {@link Maintenance#hash(String)}.
+ */
+public class HashResponse extends AbstractResponse<io.etcd.jetcd.api.HashResponse> {
+
+    /**
+     * Creates a new HashResponse from the gRPC response.
+     *
+     * @param response the gRPC hash response
+     */
+    public HashResponse(io.etcd.jetcd.api.HashResponse response) {
+        super(response, response.getHeader());
+    }
+
+    /**
+     * Returns the hash value computed from the responding member's key-value store.
+     *
+     * @return the response hash.
+     */
+    public int getHash() {
+        return getResponse().getHash();
+    }
+}
+
